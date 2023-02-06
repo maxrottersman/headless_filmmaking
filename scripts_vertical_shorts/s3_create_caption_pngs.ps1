cf8fc24f6b518fddb4999cc504ac873206417f3b
maxrottersman <maxrottersman@gmail.com>
2023-02-06 11:35:01 -0500

    new

--- scripts_vertical_shorts/s3_create_caption_pngs.ps1
+++ scripts_vertical_shorts/s3_create_caption_pngs.ps1
@@ -2,10 +2,17 @@
 # Use Imagemagick to create text captions against transparents backgrounds (PNG files)
 #
 # colors snow2, 
+#
+# captions.txt (example)
+# filenumber|captiontext
+# 1|Let's Make An Analogy For the Ukraine War
+# 2|Putin Parked Thousands of Ladas In Ukraine...
 # 
 $captions = Import-Csv .\captions.txt -delimiter "|"
+$captions_folder = "captions\"
 # https://www.educba.com/powershell-array-of-strings/
-$IMcolors = @("white","snow2","MediumOrchid1","SlateGray1","cyan1","coral","LightSlateBlue","goldenrod1","LawnGreen","DeepSkyBlue1","magenta4","","")
+# An array of random colors for variety
+$IMcolors = @("white","snow2","MediumOrchid1","SlateGray1","cyan1","coral","LightSlateBlue","goldenrod1","LawnGreen","DeepSkyBlue1","magenta4","white","snow2")
 # $($IMcolors[0])
 $idx = 1
 
@@ -22,7 +29,8 @@ $idx = 1
 	#$cmd = "magick -size 800x1600 -background none -font Open-Sans-Bold -strokewidth 2  -stroke " + $IMcolors[$idx] + "   -undercolor " + $IMcolors[$idx] + " -gravity center `"caption: $( $captiontext)\ `"  $( $filenumber)caption.png"
 	
 	# ROUNDED CORNERS STYLE (doesn't work top box, not perfect
-	$cmd = "magick -size 800x1600 -background none -font Open-Sans-Bold -strokewidth 2  -stroke " + $($IMcolors[$idx]) + "   -undercolor " + $($IMcolors[$idx]) + " -gravity center `"caption: $( $captiontext)\ `" ``( `+clone -morphology dilate disk:12 ``) `+swap -composite $( $filenumber)caption.png"
+	# 80% of 1080x1920 = 864x1536, 70% 756x1344
+	$cmd = "magick -size 864x1536 -background none -font Open-Sans-Bold -strokewidth 2  -stroke " + $($IMcolors[$idx]) + "   -undercolor " + $($IMcolors[$idx]) + " -gravity center `"caption: $( $captiontext)\ `" ``( `+clone -morphology dilate disk:12 ``) `+swap -composite $captions_folder$( $filenumber)caption.png"
 
 #
 	Write-host  $cmd
